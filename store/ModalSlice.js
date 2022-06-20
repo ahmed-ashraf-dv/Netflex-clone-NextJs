950bde3cf48292e013e73392ed3efaf9eb35b803
Ahmed Ashraf <ahmdgun0@gmail.com>
2022-06-20 03:21:58 +0200

    Update ModalSlice.js

--- store/ModalSlice.js
+++ store/ModalSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const ModalSlice = createSlice({
   name: "ModalSlice",
-  initialState: { isOpen: false, currentVideoId: "" },
+  initialState: { isOpen: false, id: null, currentVideoId: "" },
   reducers: {
     openModal: (state, { payload }) => {
       state.isOpen = true;
